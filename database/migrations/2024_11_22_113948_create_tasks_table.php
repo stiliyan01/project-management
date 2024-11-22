87b53e1002b3af06dd4f6e42a801f1c388cd63af
stiliyan <stiliyan_2001@abv.bg>
2024-11-22 13:50:04 +0200

    Add name and due_date fields to Task model; create name field in Project model

--- database/migrations/2024_11_22_113948_create_tasks_table.php
+++ database/migrations/2024_11_22_113948_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('due_date');
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
             $table->timestamps();
         });
     }
