38dee729f2df628d28a135cdd84a26876b6de34e
stiliyan <stiliyan_2001@abv.bg>
2024-11-22 14:10:59 +0200

    Add fillable properties and relationships to Project and Task models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
