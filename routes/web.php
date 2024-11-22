5f54e0001905a0a39886a022e94aa56a53d664ee
stiliyan <stiliyan_2001@abv.bg>
2024-11-22 14:11:39 +0200

    Add resource routes for Project and Task controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('projects', ProjectController::class);
+Route::resource('tasks', TaskController::class);
